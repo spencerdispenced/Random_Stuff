a1b2c28cd84f436ce4ba2e7002ae3a21cf18d260
spencerdispenced <spencerdispenced@gmail.com>
2022-01-11 01:53:58 -0500

    added min coin problem

--- minimum_coins.py
+++ minimum_coins.py
@@ -2,14 +2,15 @@
 def minimum_coins(target_value: int, coins: list[int]) -> int:
     """ Find minimum number of values 'coins' needed to make change 'target_value' """
 
-    nums = [float('inf') for i in range(target_value+1)]
-    nums[0] = 0
+    min_coins_needed = [float('inf') for i in range(target_value+1)]
+    min_coins_needed[0] = 0
 
     for coin in coins:
-        for amount in range(len(nums)):
-            if coin <= amount:
-                nums[amount] = min(nums[amount], 1 + nums[amount-coin])
-    return nums[target_value]
+        for current_target in range(len(min_coins_needed)):
+            if coin <= current_target:
+                min_coins_needed[current_target] = min(
+                    min_coins_needed[current_target], 1 + min_coins_needed[current_target-coin])
+    return min_coins_needed[target_value]
 
 
 print(minimum_coins(5, [1, 2, 3]))
