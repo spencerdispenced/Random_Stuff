8682a535d2691a0fb7c2cd5ad0273ae05552cbd8
spencerdispenced <spencerdispenced@gmail.com>
2022-07-02 20:41:39 -0400

    Added ContainsDuplicate and ValidaAnagram

---
+++ DSA/ContainsDuplicate/ContainsDuplicate.java
@@ -0,0 +1,32 @@
+package DSA.ContainsDuplicate;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicate {
+
+    public static boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> uniques = new HashSet<Integer>();
+
+        for (Integer i : nums) {
+            if (uniques.contains(i))
+                return true;
+            else
+                uniques.add(i);
+        }
+
+        return false;
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = { 1, 2, 3, 1 }; // true
+        int[] nums2 = { 1, 2, 3, 4 }; // false
+
+        System.out.println(containsDuplicate(nums));
+        System.out.println(containsDuplicate(nums2));
+
+    }
+}
